9769ad3bf5b6f5d5a15019eda764683befa4c02f
Cédric Dupont <lunarok@gmail.com>
2017-12-29 08:56:59 +0100

    Update specific.sh

--- resources/specific.sh
+++ resources/specific.sh
@@ -3,3 +3,6 @@ echo "Installation spécifique plugin"
 
 rm -rf attachments
 mkdir attachments
+
+sed -i "1407s/extension/getExtension/" resources\node_modules\mail-listener3\node_modules\mailparser\lib\mailparser.js
+# replace defaultExt = mime.extension(contentType); to defaultExt = mime.getExtension(contentType);
